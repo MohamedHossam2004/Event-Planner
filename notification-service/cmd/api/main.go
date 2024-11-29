7225022a74f69b6f6d256bbb25ddb92bb7af1d7f
mohamed-tarek183 <mohtarek183@gmail.com>
2024-11-29 17:49:08 +0200

    Mailling works for subs and unsubs event(general)

--- notification-service/cmd/api/main.go
+++ notification-service/cmd/api/main.go
@@ -4,23 +4,45 @@ import (
 	"context"
 	"log"
 	"net/http"
+	"os"
+	//"fmt"
+	"strconv"
 
 	"github.com/go-chi/chi/v5"
-
+	"github.com/MohamedHossam2004/Event-Planner/notification-service/internal/mailer"
 	// "github.com/go-chi/chi/v5/middleware"
 	"go.mongodb.org/mongo-driver/mongo"
-
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo/options"
-
-	"authentication-service\internal\mailer\mailer.go"
 )
 
+const (
+	webPort = ":8080"
+	webEnv  = "development"
+)
 var (
 	mongoClient        *mongo.Client
 	generalMailingList *mongo.Collection
 	eventMailingList   *mongo.Collection
 )
+
+type config struct {
+	port string
+	env  string
+	smtp struct {
+		host     string
+		port     int
+		username string
+		password string
+		sender   string
+	}
+}
+
+type application struct {
+	config config
+	logger *log.Logger
+	mailer mailer.Mailer
+}
 
 
 
@@ -43,7 +65,7 @@ func connectToDb() {
 
 }
 
-func subscribeGeneral(w http.ResponseWriter, r *http.Request) {
+func (app * application) subscribeGeneral(w http.ResponseWriter, r *http.Request) {
 	userEmail := r.Header.Get("Email")
 
 	var result bson.M
@@ -60,6 +82,17 @@ func subscribeGeneral(w http.ResponseWriter, r *http.Request) {
 			log.Println("Email inserted successfully!")
 			w.WriteHeader(http.StatusOK)
 			w.Write([]byte("Email successfully subscribed!"))
+			app.background(func() {
+				err := app.mailer.Send(userEmail,"SubscribeTemplate.tmpl",nil)
+				if err !=nil{
+					app.logger.Println(err)
+
+				}
+			})
+			
+
+
+
 	} else {
 		log.Println("Email already subscribed")
 		w.WriteHeader(http.StatusBadRequest)
@@ -87,7 +120,7 @@ func subscribeEvent(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func unsubscribe(w http.ResponseWriter, r *http.Request) {
+func (app * application) unsubscribe(w http.ResponseWriter, r *http.Request) {
 	userEmail := r.Header.Get("Email")
 
 	if userEmail == "" {
@@ -110,19 +143,66 @@ func unsubscribe(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("Successfully unsubscribed"))
 	log.Printf("Email '%s' has been unsubscribed", userEmail)
+	app.background(func() {
+		err := app.mailer.Send(userEmail,"UnsubscribeTemplate.tmpl",nil)
+		if err !=nil{
+			app.logger.Println(err)
+
+		}
+	})
 }
 
 func main() {
+	var cfg config
+	cfg.port = webPort
+	cfg.env = webEnv
+	cfg.smtp.host = "localhost"
+	portStr := "1025"
+	cfg.smtp.username = ""
+	cfg.smtp.password = ""
+	cfg.smtp.sender = "giu-event-hub@giu-uni.de"
+
+	if cfg.smtp.host == "" || portStr == "" {
+		log.Fatal("Environment variables for Mailhog are not set")
+	}
+	var err error
+	cfg.smtp.port, err = strconv.Atoi(portStr)
+	if err != nil {
+		log.Fatal("Error: Invalid MAILHOG_PORT value: %s\n", portStr)
+		return
+	}
+
+	logger := log.New(os.Stdout, "", log.Ldate|log.LUTC)
+	app := &application{
+		config: cfg,
+		logger: logger,
+		mailer: mailer.New(cfg.smtp.host, cfg.smtp.port, cfg.smtp.username, cfg.smtp.password, cfg.smtp.sender),
+	}
+
+	log.Printf("starting user service on %s\n", cfg.port)
+
+	
+
+	
+
+
+
+
+
+
+
 	r := chi.NewRouter()
 	connectToDb()
 
 	r.Route("/subscribe", func(r chi.Router) {
 		
-		r.Post("/general", subscribeGeneral)
+		r.Post("/general", app.subscribeGeneral)
 		r.Post("/event/{id}", subscribeEvent)
 	})
 
-	r.Post("/unsubscribe", unsubscribe)
+	r.Post("/unsubscribe", app.unsubscribe)
 
-	http.ListenAndServe(":8080", r)
+	err=http.ListenAndServe(app.config.port, r)
+
+	log.Fatal(err)
 }
